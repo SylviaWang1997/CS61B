929c61dd081e046bfc55e1aed38c58a853a30a0d
Xuran Wang <sylviaw0704@gmail.com>
2019-04-14 23:16:39 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int j=1; j< 10;j++) {
+            x = x + j;
+        }
+         System.out.print(x);
+    }
+}
